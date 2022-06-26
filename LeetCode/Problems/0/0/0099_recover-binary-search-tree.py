42b7f4cfe3d5f49b7bce54d6b9494969ed2765e9
Ahmed Yarub Hani Al Nuaimi <ahmed.alnuaimi@zwift.com>
2022-06-25 21:11:16 -0300

    LC 99

--- LeetCode/Problems/0/0/0099_recover-binary-search-tree.py
+++ LeetCode/Problems/0/0/0099_recover-binary-search-tree.py
@@ -1,31 +1,21 @@
-from typing import Optional
-
-from Utils.treeviz import TreeNode, stringToTree, drawtree
-
-
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
+        arr = []
 
-        def traverse(node: Optional[TreeNode]) -> tuple:
-            mns, mxs = [],[]
+        def inorder(node: Optional[TreeNode]) -> None:
+            if node:
+                inorder(node.left)
+                arr.append(node)
+                inorder(node.right)
 
-            for nxt in [node.left,node.right]:
-                if nxt:
-                    lres = traverse(nxt)
-                    mns.append(lres[0])
-                    mxs.append(lres[1])
+        inorder(root)
 
-            
+        wrong = []
+        for i, node in enumerate(arr):
+            if (i > 0 and node.val < arr[i - 1].val) or (i < len(arr) - 1 and node.val > arr[i + 1].val):
+                wrong.append(node)
 
-        traverse(root, TreeNode(float('-inf')), TreeNode(float('inf')))
-
-
-if __name__ == '__main__':
-    root = stringToTree('[2,3,1]')
-    drawtree(root)
-    print(Solution().recoverTree(root))
-    # print(Solution().recoverTree(stringToTree('[1,3,null,null,2]')))
-    # print(Solution().recoverTree(stringToTree('[3,1,4,null,null,2]')))
+        wrong[0].val, wrong[-1].val = wrong[-1].val, wrong[0].val
